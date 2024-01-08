53ac30b4fae53cc671ddd93447208531c2a3f572
mrznaboya <zuyax@yahoo.com>
2024-01-08 17:58:23 +0800

    Setup redux for messages

---
+++ src/store/features/messages.ts
@@ -0,0 +1,25 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+import { Message } from "../../model/message";
+
+type MessagesInitalState = {
+  [key: string]: Message;
+};
+
+const initialState: MessagesInitalState = {};
+
+export const messages = createSlice({
+  name: "messages",
+  initialState,
+  reducers: {
+    addMessages: (state, action: PayloadAction<Message[]>) => {
+      action.payload.map((message) => {
+        state[message.id] = message;
+      });
+    },
+  },
+});
+
+export const messagesActions = messages.actions;
+
+export default messages.reducer;
