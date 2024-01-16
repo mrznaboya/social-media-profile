8cdd47ba87d4253cd9b094279bfb5e37a9221497
mrznaboya <zuyax@yahoo.com>
2024-01-16 17:53:41 +0800

    Update messageThread.tsx

--- app/messageThread.tsx
+++ app/messageThread.tsx
@@ -2,12 +2,11 @@ import { useKeyboard } from "@react-native-community/hooks";
 import { router } from "expo-router";
 import React, { useEffect, useMemo, useState } from "react";
 import {
-  Keyboard,
   Platform,
   StyleSheet,
   Text,
   TextInput,
-  TouchableWithoutFeedback,
+  TouchableOpacity,
 } from "react-native";
 import Animated, {
   useSharedValue,
@@ -17,8 +16,6 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import Header from "../src/components/Header";
 import { useAppDispatch, useAppSelector } from "../src/store";
-
-const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 
 const MessageThread = () => {
   const dispatch = useAppDispatch();
@@ -30,11 +27,12 @@ const MessageThread = () => {
   const [message, setMessage] = useState("");
   const { keyboardHeight, keyboardShown } = useKeyboard();
   const inputY = useSharedValue(20);
-  /**
-   * `keyboardHeight` is 0 until the keyboard is shown for the first time.
-   * It remains as a non-zero value even after it has been closed.
-   */
+
   useEffect(() => {
+    /**
+     * `keyboardHeight` is 0 until the keyboard is shown for the first time.
+     * It remains as a non-zero value even after it has been closed.
+     */
     if (keyboardHeight !== 0 && keyboardHeight !== inputY.value) {
       inputY.value = keyboardHeight;
     } else if (!keyboardShown) {
@@ -46,7 +44,7 @@ const MessageThread = () => {
         inputY.value = 20;
       }
     }
-  });
+  }, [inputY, keyboardHeight, keyboardShown]);
 
   const animatedStyles = useAnimatedStyle(() => {
     return {
@@ -70,43 +68,31 @@ const MessageThread = () => {
 
   console.log("keyboardHeight", keyboardHeight);
   const onInputFocus = () => {
-    inputY.value = keyboardHeight;
+    // inputY.value = keyboardHeight;
     // inputY.value = withTiming(100, {
     //   duration: 200,
     //   easing: Easing.linear,
     // });
   };
 
-  const onInputBlur = () => {
-    inputY.value = 20;
-    // inputY.value = withTiming(20, {
-    //   duration: 200,
-    //   easing: Easing.linear,
-    // });
-  };
-
-  const onBackdropPressed = () => {
-    Keyboard.dismiss();
-    inputY.value = 20;
-  };
+  const send = () => {};
 
   return (
     <SafeAreaView style={styles.container} edges={["top"]}>
       <Header leftButton={{ onPress: goBack }} showLogo />
       {!existingThread && <Text>No messages yet</Text>}
 
-      <TouchableWithoutFeedback
-        onPress={onBackdropPressed}
-        style={styles.backdrop}
-      >
-        <AnimatedTextInput
+      <Animated.View style={[styles.inputContainer, animatedStyles]}>
+        <TextInput
           value={message}
           onChangeText={(text) => setMessage(text)}
-          style={[styles.input, animatedStyles]}
+          style={[styles.input]}
           onFocus={onInputFocus}
-          // onBlur={onInputBlur}
         />
-      </TouchableWithoutFeedback>
+        <TouchableOpacity onPress={send}>
+          <Text>Send</Text>
+        </TouchableOpacity>
+      </Animated.View>
     </SafeAreaView>
   );
 };
@@ -120,17 +106,24 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === "android" ? 20 : 0,
   },
   backdrop: {
-    flex: 1,
+    // flex: 1,
     height: 400,
     width: 200,
   },
   input: {
-    height: 45,
-    width: "90%",
+    height: 40,
+    width: "85%",
     alignSelf: "center",
     backgroundColor: "#fff",
     borderWidth: 1,
     borderRadius: 10,
     paddingHorizontal: 10,
+  },
+  inputContainer: {
+    height: 55,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    alignSelf: "center",
   },
 });
